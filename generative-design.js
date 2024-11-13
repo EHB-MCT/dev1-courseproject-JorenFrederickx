0fafd3a61dd0e37c8c5e779450c39433ac2799e9
Joren Frederickx <joren.frederickx@student.ehb.be>
2024-11-13 22:43:43 +0100

    course project styling

--- generative-design.js
+++ generative-design.js
@@ -5,11 +5,13 @@ import * as Utils from "../scripts/utils.js";
 draw();
 
 function draw() {
-    context.lineWidth = 5;
-
+    context.lineWidth = 2;
+    const rows = 50; // Set the number of rows of circles
+    const columns = 50; // Set the number of columns of circles
+    const spacing = 20; // Distance between circles
 
     context.fillStyle = 'black';
-    context.strokeStyle = 'white';
+    context.strokeStyle = 'red';
 
     for (let j = 0; j < rows; j++) {
         for (let i = 0; i < columns; i++) {
